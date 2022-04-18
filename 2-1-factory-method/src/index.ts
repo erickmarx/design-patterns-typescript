fe9a29e294ebcfc0d682881f26b90cbed0abc20a
Erick Marx <erick.domingos@finnet.com.br>
2022-04-18 08:44:26 -0300

    factory method example

--- 2-1-factory-method/src/index.ts
+++ 2-1-factory-method/src/index.ts
@@ -1,1 +1,16 @@
-const curso: string = "Design Patterns com TypeScript";
+import { CarTransport } from "./transport/car-transport";
+import { MotorcycleTransport } from "./transport/motorcycle-transport";
+import { Transport } from "./transport/transport";
+
+declare var process;
+let transport: Transport;
+
+if (process.argv.includes("--uber")) {
+  transport = new CarTransport();
+} else if (process.argv.includes("--log")) {
+  transport = new MotorcycleTransport();
+} else {
+  console.error("Select a argv");
+}
+
+if (transport) console.log(transport.startTransport());
